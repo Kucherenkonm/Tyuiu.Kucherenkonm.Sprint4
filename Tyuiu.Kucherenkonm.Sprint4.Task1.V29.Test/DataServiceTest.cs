a74c3ff43b87ae5a694f831ea366a1e7336aa9fe
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-11-09 21:30:23 +0500

    Task1

---
+++ Tyuiu.Kucherenkonm.Sprint4.Task1.V29.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.Kucherenkonm.Sprint4.Task1.V29.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint4.Task1.V29.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[] numsArray = { 5, 3, 3, 4, 1, 3, 3, 5, 6, 2, 7, 2, 1, 8 };
+            int res = ds.Calculate(numsArray);
+            int wait = 31;
+            Assert.AreEqual(res, wait);
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint4.Task1.V29/Program.cs
@@ -0,0 +1,44 @@
+﻿using Tyuiu.Kucherenkonm.Sprint4.Task1.V29.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (ввод с клавиатуры)                            *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                Подсчитать сумму нечетных элементов массива              *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int len;
+        Console.WriteLine("Введите количество элементов массива: ");
+        len = Convert.ToInt32(Console.ReadLine());
+        int[] numsArray = new int[len];
+        for (int i = 0; i <= len-1; i++)
+        {
+            Console.Write("Введите значение " + i + " элемента массива: ");
+            numsArray[i] = Convert.ToInt32(Console.ReadLine());
+        }
+        Console.WriteLine();
+        Console.WriteLine();
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+        int res = ds.Calculate(numsArray);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
