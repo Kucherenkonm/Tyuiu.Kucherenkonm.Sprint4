0467812cd77742ad4616e0375d4612d77e41ddc1
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-11-09 20:26:32 +0500

    Add project files.

---
+++ Tyuiu.Kucherenkonm.Sprint4.Task0.V21/Program.cs
@@ -0,0 +1,34 @@
+﻿using Tyuiu.Kucherenkonm.Sprint4.Task0.V21.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Одномерные массивы (статический ввод)                             *");
+        Console.WriteLine("* Задание #0                                                              *");
+        Console.WriteLine("* Вариант #21                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                Подсчитать сумму четных элементов массива                *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+        int[] numsArray = { 8, 2, 4, 3, 8, 5, 6, 7, 9, 8 };
+
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+
+       int res = ds.GetSumEvenArrEl(numsArray);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
