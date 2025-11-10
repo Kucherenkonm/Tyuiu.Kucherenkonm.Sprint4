25c9faf444308a48486c66e160f290c9993d7383
Надежда Кучеренко <Tyuiu.kucherenkonm@outlook.com>
2025-11-10 13:18:41 +0500

    Task4

---
+++ Tyuiu.Kucherenkonm.Sprint4.Task4.V23.Test/DataServiceTest.cs
@@ -0,0 +1,17 @@
+﻿using Tyuiu.Kucherenkonm.Sprint4.Task4.V23.Lib;
+namespace Tyuiu.Kucherenkonm.Sprint4.Task4.V23.Test
+{
+    [TestClass]
+    public sealed class DataServiceTest
+    {
+        [TestMethod]
+        public void ValidCalculate()
+        {
+            DataService ds = new DataService();
+            int[,] matrix = { { 8, 7, 5, 8, 7 }, { 8, 3, 3, 6, 4 }, { 4, 4, 5, 5, 4 }, { 3, 4, 7, 7, 3 }, { 6, 8, 3, 6, 7 } };
+            int res = ds.Calculate(matrix);
+            int wait = 65;
+            Assert.AreEqual(wait, res);
+        }
+    }
+}
---
+++ Tyuiu.Kucherenkonm.Sprint4.Task4.V23/Program.cs
@@ -0,0 +1,63 @@
+﻿using Tyuiu.Kucherenkonm.Sprint4.Task4.V23.Lib;
+internal class Program
+{
+    static void Main(string[] args)
+    {
+
+        DataService ds = new DataService();
+
+        Console.Title = "Спринт #4 | Выполнила: Кучеренко Н. М. | ИИПб - 25 - 1";
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* Спринт #4                                                               *");
+        Console.WriteLine("* Тема: Двумерные массивы (ввод с клавиатуры)                             *");
+        Console.WriteLine("* Задание #4                                                              *");
+        Console.WriteLine("* Вариант #23                                                             *");
+        Console.WriteLine("* Выполнила: Кучеренко Надежда Михайловна | ИИПб - 25 - 1                 *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* УСЛОВИЕ:                                                                *");
+        Console.WriteLine("*                 Найти сумму нечетных элементов матрицы                  *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("*                                                                         *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* ИСХОДНЫЕ ДАННЫЕ:                                                        *");
+        Console.WriteLine("***************************************************************************");
+
+        Console.WriteLine("Введите количество строк в массиве: ");
+        int rows = Convert.ToInt32(Console.ReadLine());
+
+        Console.WriteLine("Введите количество столбцов в массиве: ");
+        int columns = Convert.ToInt32(Console.ReadLine());
+
+        int[,] mtrx = new int[rows, columns];
+        Console.WriteLine("***************************************************************************");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"Введите {i},{j} элемент массива: ");
+                mtrx[i,j] = Convert.ToInt32(Console.ReadLine());
+            }
+        }
+        Console.WriteLine("Массив: ");
+
+        for (int i = 0; i < rows; i++)
+        {
+            for (int j = 0; j < columns; j++)
+            {
+                Console.Write($"{mtrx[i, j]} \t");
+            }
+            Console.WriteLine();
+        }
+
+        Console.WriteLine();
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("* РЕЗУЛЬТАТ:                                                              *");
+        Console.WriteLine("***************************************************************************");
+        Console.WriteLine("Сумма нечетных элементов: ");
+        int res = ds.Calculate(mtrx);
+
+        Console.WriteLine(res);
+        Console.ReadKey();
+    }
+}
